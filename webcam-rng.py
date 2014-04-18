2738584386e440b903f4642cd810e0ba179932b8
epitron <chris@ill-logic.com>
2014-04-18 17:07:10 -0400

    Output "random bytes" (hashes of the noise)

--- webcam-rng.py
+++ webcam-rng.py
@@ -6,6 +6,7 @@
 # Usage:
 #   1) Run:
 #        webcam-rng.py <video capture device number (default: 0)>
+#        (Note: Python opencv2 bindings must be installed)
 #
 #   2) Point your webcam at a stationary object, like a wall
 #
@@ -14,15 +15,45 @@
 
 import numpy as np
 import cv2
+import hashlib
 
-def extract_frame_noise(a, b):
-  """ Detect CCD noise """
 
-  diff          = cv2.absdiff(a, b)
-  min, max      = diff.min(), diff.max()
-  amplification = 255 / (max - min)
+def amplify_noise(noise):
+  """ Make the noise easy to see (for the visual output) """
+  min, max      = noise.min(), noise.max()
+  amplification = 255.0 / (max - min)
 
-  return diff * amplification
+  return (noise * amplification).astype("uint8")
+
+
+def extract_noise(a, b):
+  """
+  CCD noise detector
+
+  TODOs:
+    * ignore large differences (low-pass filter) to ensure
+      that noise is being captured
+  """
+  return cv2.absdiff(a, b)
+
+
+old_rands = set()
+
+def noise_to_rand(noise):
+  """ Output a stream of random bytes """
+
+  global old_rands
+
+  # TODO: This should be done in an information-theoretic way.
+  #       (eg: determine noise distribution, use chaotic mixing, etc.)
+  rand = hashlib.sha256(noise.tostring()).hexdigest()
+
+  if rand in old_rands:
+    print "Duplicate random numbers!!!"
+  else:
+    old_rands.add(rand)
+
+  return rand
 
 
 def webcam_noise(devicenum=0):
@@ -31,13 +62,15 @@ def webcam_noise(devicenum=0):
   lastframe = None
 
   while(True):
-    # Capture frame-by-frame
+    # Capture a frame
     ret, currentframe = cap.read()
 
     if lastframe is not None:
-      noise = extract_frame_noise(lastframe, currentframe)
+      noise = extract_noise(lastframe, currentframe)
 
-      cv2.imshow('Video entropy collector (q to quit)', noise)
+      cv2.imshow('Video entropy collector (q to quit)', amplify_noise(noise))
+
+      print noise_to_rand(noise) # random bytes
 
       if cv2.waitKey(1) & 0xFF == ord('q'):
         break
