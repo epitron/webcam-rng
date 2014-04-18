78e8a4821723cfd5906f6a552069bc19fc7c2155
epitron <chris@ill-logic.com>
2014-04-18 19:06:07 -0400

    Quite a few changes!

--- webcam-rng.py
+++ webcam-rng.py
@@ -5,7 +5,7 @@
 #
 # Usage:
 #   1) Run:
-#        webcam-rng.py <video capture device number (default: 0)>
+#        webcam-rng.py <video capture device number (default: 0)> [<output file for random bytes>]
 #        (Note: Python opencv2 bindings must be installed)
 #
 #   2) Point your webcam at a stationary object, like a wall
@@ -14,24 +14,23 @@
 #
 # 
 # TODOs:
-# - Statistical tests for entropy
+# ----------------
+#
+# Statistical tests for entropy
 #   => dieharder: http://www.phy.duke.edu/~rgb/General/dieharder.php
 #   => ent:       https://www.fourmilab.ch/random/
 #   => diehard:   https://en.wikipedia.org/wiki/Diehard_tests
 #   => https://en.wikipedia.org/wiki/Randomness_test
 #
+# Entropy pool
+#   => How to Eat Your Entropy and Have it Too (Fortuna): http://eprint.iacr.org/2014/167
+#   => Hash chain: https://en.wikipedia.org/wiki/Hash_chain
+#   => On entropy and randomness: http://lwn.net/Articles/261804/
+#
 
 import numpy as np
 import cv2
 import hashlib
-
-
-def amplify_noise(noise):
-  """ Make the noise easy to see (for the visual output) """
-  min, max      = noise.min(), noise.max()
-  amplification = 255.0 / (max - min)
-
-  return (noise * amplification).astype("uint8")
 
 
 def extract_noise(a, b):
@@ -45,42 +44,65 @@ def extract_noise(a, b):
   return cv2.absdiff(a, b)
 
 
-def hash_noise(noise):
-  """ Cheap stream of random bytes """
+def amplify_noise(noise):
+  """ Amplifly the noise to make it more apparent in the GUI display """
 
-  # TODO: This should be done in an information-theoretic way.
-  #       (eg: determine noise distribution, use chaotic mixing, etc.)
-  return hashlib.sha256(noise.tostring()).hexdigest()
+  min, max      = noise.min(), noise.max()
+  amplification = 255.0 / (max - min)
+
+  return (noise * amplification).astype("uint8")
 
 
-def webcam_noise(devicenum=0):
-  cap       = cv2.VideoCapture(devicenum)
-  old_rands = set()
-  lastframe = None
+def write_noise_to_file(noise, count):
+  noisefilename = "noise-%0.4d.dat" % count
 
-  while(True):
+  noisefile = open(noisefilename, "wb")
+  noisefile.write(noise)
+  noisefile.close()
+
+
+def webcam_rng(devicenum=0, rand_filename="rand.dat"):
+  hash       = hashlib.sha512()
+  cap        = cv2.VideoCapture(devicenum)
+  randfile   = open(rand_filename, "wb")
+  old_rands  = set()
+  lastframe  = None
+  framecount = 0
+  collected  = 0
+
+  while True:
     # Capture a frame
     ret, currentframe = cap.read()
 
     if lastframe is not None:
+      framecount += 1
+
       noise = extract_noise(lastframe, currentframe)
 
       cv2.imshow('Video entropy collector (q to quit)', amplify_noise(noise))
 
-      rand = hash_noise(noise) # random bytes
-      print rand
+      # write_noise_to_file(noise, framecount)
 
-      if rand in old_rands:
-        print "Duplicate random number!!!"
+      # hash the noise to generate random bytes
+      hash.update(noise.tostring())
+      randbytes = hash.digest()
+
+      collected += len(randbytes)
+      randfile.write(randbytes)
+
+      print "%0.40s... (%d bytes) -- %d collected" % (randbytes.encode("hex"), len(randbytes), collected)
+
+      if randbytes in old_rands:
+        print "!!!Duplicate random number!!!"
       else:
-        old_rands.add(rand)
-
+        old_rands.add(randbytes)
 
       if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
     lastframe = currentframe
 
+  randfile.close()
   cap.release()
   cv2.destroyAllWindows()
 
@@ -95,6 +117,12 @@ if __name__ == '__main__':
   else:
     devicenum = 0
 
-  print "Using video capture device #%d" % devicenum
+  if len(args) > 2:
+    randfile = args[2]
+  else:
+    randfile = "rand.dat"
 
-  webcam_noise(devicenum)
+  print "Using video capture device #%d" % devicenum
+  print "Writing random bytes to %s" % randfile
+
+  webcam_rng(devicenum, randfile)
