02921948d6a229d0f576b2ddc3758a50bbddf14d
epitron <chris@ill-logic.com>
2016-06-12 18:43:51 -0400

    Automatically add captured entropy to the kernel's entropy pool if the script has write permissions to /dev/random

--- webcam-rng.py
+++ webcam-rng.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+# -*- coding: utf8 -*-
 
 #
 # webcam-rng
@@ -10,6 +11,33 @@
 import numpy as np
 import cv2
 import hashlib
+import os
+import sys
+import fcntl
+import struct
+
+
+RNDADDENTROPY = 1074287107        # from /usr/include/linux/random.h
+
+def add_to_entropy_pool(data):
+    """
+    Add entropy to Linux's PRNG entropy pool.
+    """
+
+    fd = os.open("/dev/random", os.O_WRONLY)
+
+    try:
+      # struct rand_pool_info {
+      #   int entropy_count;
+      #   int buf_size;
+      #   __u32 buf[0];
+      # };
+      fmt = 'ii%is' % len(data)
+      rand_pool_info = struct.pack(fmt, 8 * len(data), len(data), data)
+      fcntl.ioctl(fd, RNDADDENTROPY, rand_pool_info)
+
+    finally:
+      os.close(fd)
 
 
 def extract_noise(frame1, frame2):
@@ -46,20 +74,28 @@ def write_noise_to_image(noise, count):
   noisefile = cv2.imwrite(filename, noise)
 
 
-def webcam_rng(devicenum=0, rand_filename="rand.dat"):
+def webcam_rng(devicenum=0, rand_filename=None):
   hash       = hashlib.sha512()
   cap        = cv2.VideoCapture(devicenum)
-  randfile   = open(rand_filename, "wb")
+
+  if rand_filename:
+    randfile = open(rand_filename, "wb")
+  else:
+    randfile = None
+
   old_rands  = set()
   lastframe  = None
   framecount = 0
   collected  = 0
+  warmup     = 10
+
+  can_write_to_dev_random = True
 
   while True:
     # Capture a frame
     ret, currentframe = cap.read()
 
-    if lastframe is not None:
+    if lastframe is not None :
       framecount += 1
 
       noise     = extract_noise(lastframe, currentframe)
@@ -70,26 +106,38 @@ def webcam_rng(devicenum=0, rand_filename="rand.dat"):
       # write_noise_to_file(noise, framecount)
       # write_noise_to_image(amp_noise, framecount)
 
-      # hash the noise to generate random bytes
-      hash.update(noise.tostring())
-      randbytes = hash.digest()
+      if framecount < warmup:
+        print "* Warming up..."
 
-      collected += len(randbytes)
-      randfile.write(randbytes)
-
-      print "%0.35s... (%d bytes) -- %d bytes generated" % (randbytes.encode("hex"), len(randbytes), collected)
-
-      if randbytes in old_rands:
-        print "!!!Duplicate random number!!!"
       else:
-        old_rands.add(randbytes)
+        # hash the noise to generate random bytes
+        hash.update(noise.tostring())
+        randbytes = hash.digest()
+        collected += len(randbytes)
+
+        if can_write_to_dev_random:
+          try:
+            add_to_entropy_pool(randbytes)
+            print "* %d bytes added to entropy pool... (%d bytes total)" % (len(randbytes), collected)
+          except IOError:
+            print "Error: Can't write to /dev/random; entropy won't be gathered."
+            can_write_to_dev_random = False
+
+        if randfile:
+          randfile.write(randbytes)
+          print "%0.35s... (%d bytes) -- %d bytes generated" % (randbytes.encode("hex"), len(randbytes), collected)
+
+        # if randbytes in old_rands:
+        #   print "!!!Duplicate random number!!!"
+        # else:
+        #   old_rands.add(randbytes)
 
       if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
     lastframe = currentframe
 
-  randfile.close()
+  # randfile.close()
   cap.release()
   cv2.destroyAllWindows()
 
@@ -110,9 +158,11 @@ if __name__ == '__main__':
   if len(args) > 2:
     randfile = args[2]
   else:
-    randfile = "rand.dat"
+    randfile = None
 
   print "Using video capture device #%d" % devicenum
-  print "Writing random bytes to %s" % randfile
+
+  if randfile:
+    print "Writing random bytes to %s" % randfile
 
   webcam_rng(devicenum, randfile)
